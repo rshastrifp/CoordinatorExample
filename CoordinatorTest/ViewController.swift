b3ecd52e69ec3351617143c48a9387665c1624e0
ron <ron@rons-macbook-air.local>
2019-01-19 23:41:42 -0500

    Initial Commit

---
+++ CoordinatorTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorTest
+//
+//  Created by ron on 2019-01-19.
+//  Copyright © 2019 ron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
